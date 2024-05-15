8c0cf9d28247e46c93d5c4fe8ac39964ff956e14
Bryan Borgeson <bryborge@gmail.com>
2024-05-14 22:49:58 -0700

    Fix: Apply custom styles to sign in/out/etc pages, and user settings pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: {
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
 
   namespace :users do
     resource :profile, only: %i[show edit update]
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.

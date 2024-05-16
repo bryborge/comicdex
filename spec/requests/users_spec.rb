b78ec049baae0b1777633742241acc57522af0a4
Bryan Borgeson <bryborge@gmail.com>
2024-05-16 00:45:26 -0700

    Tests: Add more request coverage

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -68,6 +68,56 @@ RSpec.describe 'Users' do
   end
 
   # User > Settings > Account Page
+  describe 'GET /users/edit' do
+    context 'when the user is not authenticated' do
+      it 'redirects to the login page' do
+        sign_out user
+
+        get edit_user_registration_path
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    context 'when the user is authenticated' do
+      it 'renders the edit template' do
+        sign_in user
+
+        get edit_user_registration_path
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+
+  describe 'PATCH /users' do
+    context 'when the user is authenticated' do
+      it "updates the user's email and redirects to the home page" do
+        sign_in user
+
+        patch user_registration_path, params: { user: { email: 'newemail@example.com', current_password: 'password' } }
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(root_path)
+        expect(user.reload.email).to eq('newemail@example.com')
+      end
+
+      it 'renders the edit template when the update fails' do
+        sign_in user
+
+        patch user_registration_path, params: { user: { email: nil } }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context 'when the user is not authenticated' do
+      it 'redirects to the login page' do
+        sign_out user
+
+        patch user_registration_path, params: { user: { email: 'newemail@example.com', current_password: 'password' } }
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
 
   # "Your Profile"
   describe 'GET /users/profile' do

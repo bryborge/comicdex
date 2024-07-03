101187a426a22f78c5e90bca72dabaa17d796214
Bryan Borgeson <bryborge@gmail.com>
2024-07-02 19:50:51 -0700

    Add: comicbook cover art

--- app/models/comicbook.rb
+++ app/models/comicbook.rb
@@ -5,6 +5,8 @@ class Comicbook < ApplicationRecord
   has_many :user_comicbooks, dependent: :destroy
   has_many :users, through: :user_comicbooks
 
+  has_one_attached :cover_image
+
   belongs_to :series
 
   validates :format, presence: true
